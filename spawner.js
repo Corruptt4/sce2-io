921bd76edcfea4a3cbd89c707d74b6bfae3bac19
Corruptt4 <corruptt4@gmail.com>
2025-03-22 06:18:03 +0200

    Update spawner.js

---
+++ spawner.js
@@ -0,0 +1,8 @@
+class Spawner {
+    constructor(currentPolys, maxPoly, mapSize, maxSides) {
+        this.currentPolys = currentPolys
+        this.maxPoly = maxPoly
+        this.mapSize = mapSize
+        this.maxSides = maxSides
+    }
+}
